3f2fdb86b64fbd4ef11a27d591f7dca1fec23ed0
smustiph <smustiph@nd.edu>
2017-07-10 14:28:12 -0400

    can decode XCTU logfile

--- decodeXCTU.py
+++ decodeXCTU.py
@@ -3,17 +3,21 @@ import os
 
 '''
 Save log file by clicking save button in XCTU
+Save it to directory the script is in
 Run this program: python decodeXCTU.py logfilename outputfile
 '''
+
 filename = sys.argv[1]
 newfilename = sys.argv[2]
 newfile = open(newfilename, 'w')
 
-with open(filename) as f:
+with open(filename, "r") as f:
+	next(f)
+	next(f)
 	for line in f:
-		line = line.strip()
-		newfile.write(bytes.fromhex(line).decode('utf-8'))
-		# print(bytes.fromhex(line).decode('utf-8'))
+		line = line.strip().split(",")
+		if line[2] == 'RECV':
+			newfile.write(bytes.fromhex(line[3]).decode('utf-8'))
 
 	# Cleaning up the files
 	f.close()
